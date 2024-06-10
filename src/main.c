0525849ae98f4ad655580906b18a243f5e217416
jkr11 <113939131+jkr11@users.noreply.github.com>
2024-06-10 16:26:58 +0200

    parsing works but needs to be refactored

--- src/main.c
+++ src/main.c
@@ -10,7 +10,7 @@
 static void usage(const char *prog_name) {
     fprintf(stderr, "Usage: %s [options] <input_file>\n", prog_name);
     fprintf(stderr, "Options:\n");
-    fprintf(stderr, "-c <Zahl> / --cycles <Zahl>     Die Anzahl der Zyklen, die simuliert werden sollen.\n");
+    fprintf(stderr, "-c <Zahl> / --cycles <Zahl>     Die Anzahl der Zyklen, die simuliert x^ sollen.\n");
     fprintf(stderr, "--cacheline-size <Zahl>         Die Größe einer Cachezeile in Byte.\n");
     fprintf(stderr, "--l1-lines <Zahl>               Die Anzahl der Cachezeilen des L1 Caches.\n");
     fprintf(stderr, "--l2-lines <Zahl>               Die Anzahl der Cachezeilen des L2 Caches.\n");
@@ -23,7 +23,70 @@ static void usage(const char *prog_name) {
 
 }
 
+
+
 int main(int argc, char* argv[]) {
-    usage(argv[0]);
+
+    int opt;
+    int c,cs,l1l,l2l,l1la,l2la,ml; // TODO: refactor this into struct
+    char *tf;
+    // probably forgot something 
+    static struct option long_options[] = {
+        {"cycles", required_argument, 0, 'c'},
+        {"cacheline-size", required_argument, 0, 1},
+        {"l1-lines", required_argument, 0, 2},
+        {"l2-lines", required_argument, 0, 3},
+        {"l1-latency", required_argument, 0, 4},
+        {"l2-latency", required_argument, 0, 5},
+        {"memory-latency", required_argument, 0, 6},
+        {"tf", required_argument, 0, 7},
+        {"help", no_argument, 0, 'h'},
+        {0,0,0,0}
+    };
+
+    while((opt = getopt_long(argc, argv, "c:h", long_options, NULL)) != -1) {
+        switch (opt)
+        {
+        case 'c':
+            c = atoi(optarg);
+            printf("cycles: %d\n", c);
+            break;
+        case 1:
+            cs = atoi(optarg);
+            printf("cs: %d\n", cs);
+            break;
+        case 2:
+            l1l = atoi(optarg);
+            printf("l1l: %d\n", l1l);
+            break;
+        case 3:
+            l2l = atoi(optarg);
+            printf("l2l: %d\n", l2l);
+            break;
+        case 4:
+            l1la = atoi(optarg);
+            printf("l1la: %d\n", l1la);
+            break;
+        case 5:
+            l2la = atoi(optarg);
+            printf("l2la: %d\n", l2la);
+            break;
+        case 6:
+            ml = atoi(optarg);
+            printf("ml: %d\n", ml);
+            break;
+        case 7:
+            tf = optarg;
+            printf("tf: %s\n", tf);
+            break;
+        case 'h':
+            usage(argv[0]);
+            exit(EXIT_SUCCESS);
+        default:
+            usage(argv[0]);
+            exit(EXIT_FAILURE);
+            break;
+        }
+    }
     exit(EXIT_SUCCESS);
 }
