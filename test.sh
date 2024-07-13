d026bc82866b4bb0b231e7c2c7bb79fa13a1e85f
jkr11 <113939131+jkr11@users.noreply.github.com>
2024-07-13 18:31:12 +0200

    add testing for random offset

--- test.sh
+++ test.sh
@@ -6,7 +6,9 @@ make clean
 echo "Compiling the project"
 make project
 
+echo "Generating cache accesses"
+python3 input.py 16
 
-echo "Running project"
-
-./project -c 10000000 --cacheline-size 16 --l1-lines 128 --l2-lines 256 --l1-latency 1 --l2-latency 3 --memory-latency 10 --tf=tracefile cache_accesses.csv
+config = "-c 10000000 --cacheline-size 16 --l1-lines 128 --l2-lines 256 --l1-latency 1 --l2-latency 3 --memory-latency 10 --tf=tracefile"
+echo "Running with "
+./project $config cache_accesses.csv
