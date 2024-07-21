4343d14b7a3d35ee90e8d14074089d34baf25b3d
jkr11 <113939131+jkr11@users.noreply.github.com>
2024-07-21 20:32:35 +0200

    cleanup

--- test.sh
+++ test.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-./project -c 10000000 --cacheline-size 16 --l1-lines 32 --l2-lines 128 --l1-latency 4 --l2-latency 16 --memory-latency 400 --tf=tracefile cache_accesses_1.csv
-
+./project -c 10000000 --cacheline-size 16 --l1-lines 32 --l2-lines 128 --l1-latency 4 --l2-latency 16 --memory-latency 400 --tf=tracefile examples/c.csv
